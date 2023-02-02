883998f50c240eb89cdaa1bc47525e2018cbe2e9
Benjamin Adolph <benjamin.adolph@capgemini.com>
2023-02-02 10:20:55 +0100

    add basic components and meta tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-        <p>Homepage</p>
+        <>
+    	    <Header />
+          <Nav />
+          <Main />
+          <Footer />
+        </>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header>
+            <img src="img/Logo.svg" alt="Logo"/>
+        </header>
+    );
+};
+
+export default Header;
